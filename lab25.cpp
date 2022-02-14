0d03733215152311a2ea9e07c7ef60a1091c1c18
Panyapat Wongdee <panyapat12345@gmail.com>
2022-02-15 02:00:52 +0700

    lab25.cpp completed

--- lab25.cpp
+++ lab25.cpp
@@ -28,7 +28,65 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real - imag*c.imag, real*c.imag + c.real*imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	ComplexNumber s;
+	s = ComplexNumber(real, imag) * ComplexNumber(c.real, -c.imag);
+	return ComplexNumber(s.real/(pow(c.real, 2) + pow(c.imag, 2)), 
+						 s.imag/(pow(c.real, 2) + pow(c.imag, 2)));
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real, 2) + pow(imag, 2));
+}
+
+double ComplexNumber::angle(){
+	double arc = atan2(imag, real);		
+	arc = (arc/3.14159265) * 180;			
+	return arc;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag)		return true;
+	else										return false;
+}
+
 //Write your code here
+
+ostream & operator<<(ostream &os, const ComplexNumber &x){
+	if(x.real != 0 && x.imag > 0)			return os << x.real << "+" << x.imag << "i" ;
+	else if(x.real != 0 && x.imag < 0)		return os << x.real << x.imag << "i" ;
+	else if(x.real == 0 && x.imag != 0)		return os << x.imag << "i" ;
+	else if(x.real != 0 && x.imag == 0)		return os << x.real ;
+	else									return os << "0" ;
+}
+
+ComplexNumber operator+(double x, const ComplexNumber &y){
+	return ComplexNumber(x+y.real, y.imag);
+}
+
+ComplexNumber operator-(double x, const ComplexNumber &y){
+	return ComplexNumber(x-y.real, -y.imag);
+}
+
+ComplexNumber operator*(double x, const ComplexNumber &y){
+	return ComplexNumber(x*y.real, x*y.imag);
+}
+
+ComplexNumber operator/(double x, const ComplexNumber &y){
+	ComplexNumber s;
+	s = x * ComplexNumber(y.real, -y.imag);
+	return ComplexNumber(s.real/(pow(y.real, 2) + pow(y.imag, 2)),
+						 s.imag/(pow(y.real, 2) + pow(y.imag, 2)));
+}
+
+bool operator==(double x, const ComplexNumber &y){
+	if(x == y.real && y.imag == 0)		return true;
+	else								return false;
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
@@ -70,8 +128,3 @@ int main(){
 	cout << (ComplexNumber(1,1) == 1) << "\n";
 	cout << (0 == ComplexNumber()) << "\n";
 }
-
-
-
-
-
